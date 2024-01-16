5a591d3bb9bfa8b6a4841e00ede5a60247306f80
2ralumin <2ralumin@naver.com>
2024-01-16 14:19:43 +0900

    feat(TracksListResponse) : 트랙 정보를 담기 위한 list dto 생성

---
+++ back/src/main/java/kodong/spotimate/music/album/dto/response/TracksListResponse.java
@@ -0,0 +1,20 @@
+package kodong.spotimate.music.album.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TracksListResponse {
+    private int total;
+    private int offset;
+    private int limit;
+    private List<TrackResponse> items;
+}
