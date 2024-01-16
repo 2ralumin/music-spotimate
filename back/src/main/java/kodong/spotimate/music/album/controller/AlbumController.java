5eafe29d778572456e63b1c2a375a009179ecde8
2ralumin <2ralumin@naver.com>
2024-01-16 16:56:19 +0900

    feat(AlbumController): 트랙 api 분리 후 controller 생성

--- back/src/main/java/kodong/spotimate/music/album/controller/AlbumController.java
+++ back/src/main/java/kodong/spotimate/music/album/controller/AlbumController.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import kodong.spotimate.music.album.dto.response.AlbumDetailResponse;
+import kodong.spotimate.music.album.dto.response.TracksListResponse;
 import kodong.spotimate.music.album.service.AlbumService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,12 +25,26 @@ public class AlbumController {
     private final AlbumService albumService;
 
     @GetMapping("/api/music/album/{id}")
-    public ResponseEntity<AlbumDetailResponse> getAlbumDetail(@PathVariable String id, HttpServletRequest httpServletRequest) throws JsonProcessingException {
+    public ResponseEntity<AlbumDetailResponse> getAlbumDetail(@PathVariable String id,
+                                                              @RequestParam String market,
+                                                              HttpServletRequest httpServletRequest) throws JsonProcessingException {
 
         String accessToken = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
         HttpHeaders httpHeaders = albumService.makeHeader(accessToken);
-        AlbumDetailResponse response = albumService.getAlbumDetail(id, httpHeaders);
+        AlbumDetailResponse response = albumService.getAlbumDetail(id, market,httpHeaders);
 
+        return ResponseEntity.ok().body(response);
+    }
+
+    @GetMapping("/api/music/album/{id}/tracks")
+    public ResponseEntity<TracksListResponse> getAlbumTracks(@PathVariable String id,
+                                                             @RequestParam String market,
+                                                             @RequestParam int limit,
+                                                             @RequestParam int offset,
+                                                             HttpServletRequest httpServletRequest) throws JsonProcessingException {
+        String accessToken = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        HttpHeaders httpHeaders = albumService.makeHeader(accessToken);
+        TracksListResponse response = albumService.getTracks(id, market, limit, offset, httpHeaders);
         return ResponseEntity.ok().body(response);
     }
 }
